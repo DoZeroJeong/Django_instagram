979d508d695be074af35bd0f0f7d9a3215899e27
DoZeroJeong <dogug2865@gmail.com>
2019-10-22 03:18:37 +0900

    Django instagram

---
+++ photo/views.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic import DetailView
+from .models import Photo
+# Create your views here.
+
+@login_required
+def photo_list(request):
+
+    photos = Photo.objects.all()
+    return render(request, 'photo/list.html', {'photos': photos})
+
+
+class PhotoUploadView(LoginRequiredMixin, CreateView):
+    model = Photo
+    fields = ['photo', 'text']
+    template_name = 'photo/upload.html'
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+        if form.is_valid():
+            form.instance.save()
+            return redirect('/')
+        else:
+            return self.render_to_response({'form': form})
+
+
+class PhotoDeleteView(LoginRequiredMixin, DeleteView):
+    model = Photo
+    success_url = '/'
+    template_name = 'photo/delete.html'
+
+
+class PhotoUpdateView(LoginRequiredMixin, UpdateView):
+    model = Photo
+    fields = ['photo', 'text']
+    template_name = 'photo/update.html'
+
+
+class PhotoDetailView(LoginRequiredMixin, DetailView):
+    model = Photo
+    template_name = 'photo/detail.html'
